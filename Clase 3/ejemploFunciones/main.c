4c1575a804848e74bb49b0756c5d8b1e84531100
mauriciofloresgonales <mlgf2000@hotmail.com>
2019-08-27 11:20:22 -0300

    menu agregado

--- Clase 3/ejemploFunciones/main.c
+++ Clase 3/ejemploFunciones/main.c
@@ -3,29 +3,53 @@
 
 //PROTOTIPO
 
+int pedirNumero(int);
 int sumarNuemeros(int,int);
+int restarNumeros(int,int);
 
 int main()
 {
     //LLAMADA
 
-    int numeroUno = 8;
-    int numeroDos = 9;
-    int total;
+    int numeroUno /*= 8*/;
+    int numeroDos /*= 9*/;
+    int totalSuma;
+    int totalResta;
 
-    total = sumarNuemeros(numeroUno,numeroDos);
-    printf("la suma es %d", total);
+    numeroUno = pedirNumero(numeroUno);
+    numeroDos = pedirNumero(numeroDos);
+    totalSuma = sumarNuemeros(numeroUno,numeroDos);
+    totalResta = restarNumeros(numeroUno,numeroDos);
+    printf("la suma es %d\n", totalSuma);
+    printf("la resta es %d\n", totalResta);
 
     return 0;
 }
 
 //DESARROLLO
 
+int pedirNumero(int numeroIngresado)
+{
+    int numero;
+    printf("ingrese un numero: ");
+    scanf("%d",&numero);
+    return numero;
+}
+
   int sumarNuemeros(int a, int b)
   {
       int resultado;
 
       resultado = a + b ;
+
+      return resultado;
+  }
+
+  int restarNumeros(int a,int b)
+  {
+      int resultado;
+
+        resultado = a - b ;
 
       return resultado;
   }
