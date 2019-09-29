83501caa2ff0d28a961cf4acda2b911b74625448
MauriciofloresGonales <mlgf2000@hotmail.com>
2019-09-29 15:45:40 -0300

    agregue bibliotecas a la carpeta .h

--- Inputs/Inputs.h
+++ Inputs/Inputs.h
@@ -1,4 +1,7 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 /** * \brief Solicita un número al usuario y lo valida
 * \param input Se carga el numero ingresado
